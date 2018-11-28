292e4e072fcbcebb08b87f6124068fb6c56e615a
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2018-11-28 13:26:44 +0100

    3 players, can place nodes

---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeScript : MonoBehaviour {
+
+    private SphereCollider zoneCol;
+    private GameObject zone;
+    public string owner;
+
+    public Color p1Color, p2Color, p3Color;
+
+	// Use this for initialization
+	void Start () {
+        zone = transform.GetChild(0).gameObject;
+        zoneCol = zone.GetComponent<SphereCollider>();
+        StartCoroutine(Grow());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    IEnumerator Grow()
+        {
+        for (float f = 1f; f < 20f; f = f + 0.1f)
+            {
+            //zoneCol.radius = f;
+            zone.transform.localScale = new Vector3(f, f, f);
+            yield return new WaitForSeconds(0.1f);
+            }
+        }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private string horizontal, vertical, fire;
+    public enum Player { Player1, Player2, Player3 };
+    public Player currentPlayer = Player.Player1;
+
+    public GameObject node;
+
+    public float movementSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        switch (currentPlayer)
+            {
+            case Player.Player1:
+                gameObject.name = "Player1";
+                horizontal = "Player1Horizontal";
+                vertical = "Player1Vertical";
+                fire = "Player1Fire";
+                break;
+
+            case Player.Player2:
+                gameObject.name = "Player2";
+                horizontal = "Player2Horizontal";
+                vertical = "Player2Vertical";
+                fire = "Player2Fire";
+                break;
+
+            case Player.Player3:
+                gameObject.name = "Player3";
+                horizontal = "Player3Horizontal";
+                vertical = "Player3Vertical";
+                fire = "Player3Fire";
+                break;
+            }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(Input.GetAxis(horizontal), 0, Input.GetAxis(vertical)) * movementSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown(fire))
+            {
+            GameObject nodeCurrent;
+            nodeCurrent = Instantiate(node, new Vector3(transform.position.x, 0, transform.position.z), transform.rotation);
+            nodeCurrent.GetComponent<NodeScript>().owner = gameObject.name; 
+            }
+	}
+}
