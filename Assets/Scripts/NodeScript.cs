0245896cf60ac4ff386c5ece6f79343bfeb77860
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2018-12-01 23:35:24 +0100

    object spawner

--- Assets/Scripts/NodeScript.cs
+++ Assets/Scripts/NodeScript.cs
@@ -15,7 +15,16 @@ public class NodeScript : MonoBehaviour {
 
     public Color p1Color, p2Color, p3Color;
 
+    public GameObject environmentAsset1, environmentAsset2, environmentAsset3;
+    private List<GameObject> assets = new List<GameObject>();
+    private GameObject currentObject;
+    public float assetYOffset = -1f;
+    public float maxObjects = 5;
+    public float delayMin = 2f, delayMax = 5f;
+    //private float placedObjects; 
+
     private IEnumerator grow;
+    private IEnumerator objects;
 
 	// Use this for initialization
 	void Start () {
@@ -24,13 +33,18 @@ public class NodeScript : MonoBehaviour {
         zoneCol = zone.GetComponent<SphereCollider>();
 
         grow = Grow();
+        objects = Objects();
         StartCoroutine(grow);
-	}
+        StartCoroutine(objects);
+        assets.Add(environmentAsset1);
+        assets.Add(environmentAsset2);
+        assets.Add(environmentAsset3);
+        }
 	
 	// Update is called once per frame
 	void Update () {
-        
-	}
+        currentObject.transform.position = new Vector3(currentObject.transform.position.x, Mathf.Lerp(currentObject.transform.position.y, 1, 10 * Time.deltaTime), currentObject.transform.position.z);
+        }
 
     IEnumerator Grow()
         {
@@ -49,6 +63,23 @@ public class NodeScript : MonoBehaviour {
     public void StopGrowing()
         {
         StopCoroutine(grow);
+        }
+
+    IEnumerator Objects()
+        {
+        for (float f = 0; f <= maxObjects; f++)
+            { 
+            yield return new WaitForSeconds(Random.Range(delayMin, delayMax));
+
+            float area = zone.transform.localScale.x - 1;
+            Vector3 spawnPos = new Vector3(Random.Range((area/4)*-1, area / 4), assetYOffset, Random.Range((area/4)*-1, area / 4));
+            currentObject = Instantiate(assets[Random.Range(0, 3)], transform, false);
+            currentObject.transform.localPosition = spawnPos;
+            //placedObjects++;
+            //currentObject.transform.position = new Vector3(currentObject.transform.position.x, Mathf.Lerp(currentObject.transform.position.y, 1, 10 * Time.deltaTime), currentObject.transform.position.z);
+            //yield return new WaitForSeconds(delayMin);
+            }
+            
         }
 
     private void OnTriggerStay(Collider other)
